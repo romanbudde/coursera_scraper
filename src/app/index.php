eabc7484f784aaeba13547e4748b78e467eff8e6
Roman Budde <roman_budde@hotmail.com>
2022-11-13 11:24:38 -0300

    removed line

--- src/app/index.php
+++ src/app/index.php
@@ -1,20 +1,11 @@
 <html lang="en">
-    <!-- DEfines languages of content : English -->
     <head>
-    <!-- Information about website and creator -->
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <!-- Defines the compatibility of version with browser -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- for make website responsive -->
     <meta name="author" content="Mr.X">
     <meta name="Linkedin profile" content="WWW.linkedin.com/Mr.X_123" >
-    <!-- To give information about author or owner -->
-    <meta name="description " 
-    content="A better place to learn computer science">
-    <!-- to explain about website in few words -->
-    <title>GeeksforGeeks</title>
-    <!-- Name of website or content to display -->
+    <title>Coursera Scraper</title>
 </head>
 <body>
     <!-- Main content of website -->
